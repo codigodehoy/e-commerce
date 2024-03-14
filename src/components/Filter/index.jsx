c072ec052f4df762f29d9ffb6ba5fc108a21d44e
David Cadena <david.cadena@hotmail.com>
2024-03-13 23:00:58 -0500

    fix: Resolve filter by rating

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,23 +1,22 @@
-import { TypeFilter } from './TypeFilter'
-import { RatingFilter } from './RatingFilter'
-import './Filter.css'
+import { TypeFilter } from "./TypeFilter";
+import { RatingFilter } from "./RatingFilter/RatingFilter";
+import "./Filter.css";
 
-function Filter () {
- 
-    return (
-        <div className='FilterContainer'>
-            <TypeFilter
-                name='Category'
-                options={[
-                    {id: "men's clothing", label: "Men's Clothing"},
-                    {id: "women's clothing", label: "Women's Clothing"},
-                    {id: "jewelery", label: "Jewelery"},
-                    {id: "electronics", label: "Electronics"}
-                ]}
-            />
-            <RatingFilter />
-        </div>
-    )
+function Filter() {
+  return (
+    <div className="FilterContainer">
+      <TypeFilter
+        name="Category"
+        options={[
+          { id: "men's clothing", label: "Men's Clothing" },
+          { id: "women's clothing", label: "Women's Clothing" },
+          { id: "jewelery", label: "Jewelery" },
+          { id: "electronics", label: "Electronics" },
+        ]}
+      />
+      <RatingFilter />
+    </div>
+  );
 }
 
-export { Filter }
+export { Filter };
